18230dfe21c88ed19de43454f68dd2fcd6c3e51a
Rich Downie <rich@richdownie.com>
2010-07-19 21:51:09 -0400

    Removing dependency on paperclip

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,6 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem "paperclip", :version => ">= 2.3.1.1"
   config.gem "nokogiri", :version => ">= 1.4.1"
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
