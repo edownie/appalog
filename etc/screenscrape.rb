5a18b86fbdd88adece9c589905337f857790e167
Eric Downie <ejm5144@verizon.net>
2011-04-20 20:56:10 -0500

    scrape

--- etc/screenscrape.rb
+++ etc/screenscrape.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
 
-doc = Nokogiri::HTML(open("http://twitter.com/cloudalog"))
+doc = Nokogiri::HTML(open("http://twitter.com/gridlaborate"))
 
 doc.css('.entry-content').each do |link|
   puts link.content
